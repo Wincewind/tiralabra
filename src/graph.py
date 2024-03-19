e4dece16405dba2066693efba9297fcf67051100
Wincewind <86107954+Wincewind@users.noreply.github.com>
2024-03-19 17:38:05 +0200

    add graph and node classes

---
+++ src/graph.py
@@ -0,0 +1,58 @@
+class Node:
+    """Solmu-luokka.
+    Obstacle on True jos kyseessä on este ja False kun solmuun voidaan kulkea.
+    Coordinates kertoo solmun x,y position pikselikartassa.
+    """
+
+    def __init__(self, obstacle: str, coordinates: tuple) -> None:
+        self.obstacle = True
+        if obstacle in [".", "G", "S"]:
+            self.obstacle = False
+        self.coords = coordinates
+
+
+class Graph:
+    """Verkko-luokka.
+    Nodes on dict-typpiä, missä (x,y)-koordinatti on avain tuple muodossa
+    ja arvoina lista naapureista ja suunnista. suunnat on numeroitu 0-7
+    myötäpäivään alkaen suoraan ylöspäin.
+    """
+
+    def __init__(self) -> None:
+        self.nodes = {}
+
+    def generate_graph(self, ascii_graph: list):
+        """Muodostetaan Graph-olio taulukosta ASCII merkkejä.
+        Merkkien oletetaan olevan Moving AI Labin materiaalin mukaisia:
+        https://www.movingai.com/benchmarks/formats.html
+
+        Args:
+             ascii_graph (list): kaksiulotteinen lista ascii merkkejä.
+        """
+        self.nodes = {}
+        directions = {
+            0: (0, -1),
+            1: (1, -1),
+            2: (1, 0),
+            3: (1, 1),
+            4: (0, 1),
+            5: (-1, 1),
+            6: (-1, 0),
+            7: (-1, -1),
+        }
+        for y, row in enumerate(ascii_graph):
+            for x, col in enumerate(row):
+                node = Node(col, (x, y))
+                if node.obstacle:
+                    continue
+                self.nodes[node.coords] = []
+                for d, mod in directions.items():
+                    neighbour_coords = (x + mod[0], y + mod[1])
+                    if 0 <= neighbour_coords[0] < len(
+                        row
+                    ) and 0 <= neighbour_coords[1] < len(ascii_graph):
+                        neighbour = Node(
+                            ascii_graph[neighbour_coords[1]][neighbour_coords[0]],
+                            neighbour_coords,
+                        )
+                        self.nodes[node.coords].append((neighbour, d))
---
+++ src/tests/graph_test.py
@@ -0,0 +1,44 @@
+import unittest
+from graph import Graph
+
+class TestGraph(unittest.TestCase):
+    def setUp(self):
+        self.graph = Graph()
+        self.test_graph_1 = [['.','.','.'],
+                             ['S','G','.'],
+                             ['S','.','.']]
+        
+        self.test_graph_2 = [['.','@','.'],
+                             ['S','G','.'],
+                             ['.','.','.']]
+        
+        self.test_graph_3 = [['.','.','.'],
+                             ['@','G','.'],
+                             ['S','.','.']]
+        
+        self.test_graph_4 = [['.','.','.','.','.','.','.'],
+                             ['.','.','.','.','.','.','.'],
+                             ['@','.','.','.','.','@','G'],
+                             ['S','.','.','.','.','.','.'],
+                             ['@','.','.','.','.','.','.']]
+        
+        self.test_graph_5 = [['.','.','.','.','.','G','.'],
+                             ['.','.','.','.','.','@','.'],
+                             ['.','.','.','.','.','@','.'],
+                             ['S','@','.','.','.','@','.'],
+                             ['.','@','.','.','.','.','.']]
+        
+        self.test_graph_6 = [['.','@','.'],
+                             ['@','G','@'],
+                             ['S','@','.']]
+
+    def test_graph_generation(self):
+        self.graph.generate_graph(self.test_graph_5)
+        neighbours = self.graph.nodes[(4,2)]
+        neighbours = [n for n in neighbours if n[0].obstacle]
+        self.assertEqual(len(neighbours),3)
+
+        self.graph.generate_graph(self.test_graph_5)
+        neighbours = self.graph.nodes[(4,2)]
+        neighbours = [n for n in neighbours if n[0].obstacle]
+        self.assertEqual(len(neighbours),3)
