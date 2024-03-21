ae5ca513edabc4b81ce7d785914860a120fb2439
Wincewind <86107954+Wincewind@users.noreply.github.com>
2024-03-21 15:05:20 +0200

    Format test file.

--- src/tests/main_test.py
+++ src/tests/main_test.py
@@ -1,10 +1,5 @@
 import unittest
-from random import randint, choice
-import os
 from main import main
-import assets_io
-from graph import Graph
-from algorithms.dijkstra import dijkstra
 
 
 class StubIO:
@@ -24,10 +19,18 @@ class TestMain(unittest.TestCase):
     def test_running_main_program_for_one_scen(self):
         io = StubIO(["AR0413SR", "1", "777"])
         main(io)
-        self.assertTrue(io.outputs[2].startswith("Skenaarion 777 ratkaisemisessa kului Dijkstran algoritmilla:"))
+        self.assertTrue(
+            io.outputs[2].startswith(
+                "Skenaarion 777 ratkaisemisessa kului Dijkstran algoritmilla:"
+            )
+        )
 
     def test_running_main_program_for_ten_random_scens(self):
         io = StubIO(["AR0413SR", "2", "1"])
         main(io)
         print(io.outputs)
-        self.assertTrue(io.outputs[-1].startswith("Dijkstran algoritmilla kesti polunetsinnässä kokonaisuudessaan:"))
+        self.assertTrue(
+            io.outputs[-1].startswith(
+                "Dijkstran algoritmilla kesti polunetsinnässä kokonaisuudessaan:"
+            )
+        )
