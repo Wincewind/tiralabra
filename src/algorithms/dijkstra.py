2cfd55b1c1e348ef06fb3de7747f14bb6d6d53c8
Wincewind <86107954+Wincewind@users.noreply.github.com>
2024-03-21 14:12:32 +0200

    Adjust distance calculation so that all scenario tests will work correctly

--- src/algorithms/dijkstra.py
+++ src/algorithms/dijkstra.py
@@ -1,7 +1,8 @@
 from heapq import heappush, heappop
-from math import sqrt
-from graph import Graph, Node # noqa # pylint: disable=unused-import
+from graph import Graph, Node  # noqa # pylint: disable=unused-import
+
 # Node importataan tässä, jotta sen itellisense:iin päästään koodissa käsiksi.
+
 
 def dijkstra(start: tuple, goal: tuple, graph: Graph):
     """
@@ -10,18 +11,23 @@ def dijkstra(start: tuple, goal: tuple, graph: Graph):
     Löydetty reitti täytetään Graph-olion visited attribuuttiin.
     """
     queue = []
-    heappush(queue, (0,start))
+    heappush(queue, (0, start))
+    graph.visited[start] = (0, start)
     while len(queue) > 0:
         distance, coords = heappop(queue)
         if goal == coords:
             break
-        for neighbour,direction in graph.nodes[coords]:
+        for neighbour, direction in graph.nodes[coords]:
             # Kuljettua matkaa täytetään vaaka tai pystysuunnassa
             # yhdellä ja diagonaalisessa suunnassa kahden neliöjuurella
-            to_neighbour = distance + 1 if direction % 2 == 0 else distance + sqrt(2)
-            if neighbour.obstacle or \
-            (neighbour.coords in graph.visited and
-             graph.visited[neighbour.coords][0] <= to_neighbour):
+            # 8 desimaalin tarkkuudella. Moving AI Labin skenaarioiden lyhyimmät
+            # polut on annettu tällä tarkkuudella, joten tällä pyritään saamaan
+            # mahdollisimman yhtenäisiä tuloksia.
+            to_neighbour = distance + 1 if direction % 2 == 0 else distance + 1.41421356
+            if neighbour.obstacle or (
+                neighbour.coords in graph.visited
+                and graph.visited[neighbour.coords][0] <= to_neighbour
+            ):
                 continue
             graph.visited[neighbour.coords] = (to_neighbour, coords)
             heappush(queue, (to_neighbour, neighbour.coords))
--- src/tests/assets_io_test.py
+++ src/tests/assets_io_test.py
@@ -1,5 +1,5 @@
 import unittest
-from random import randint, shuffle
+from random import randint, choice
 import os
 import assets_io
 from graph import Graph
@@ -8,7 +8,6 @@ from algorithms.dijkstra import dijkstra
 class TestAssetsIO(unittest.TestCase):
     def setUp(self):
         self.map_name = "AR0413SR"
-        
 
     def test_reading_map(self):
         ascii_map = assets_io.read_map(self.map_name)
@@ -30,18 +29,17 @@ class TestAssetsIO(unittest.TestCase):
 
     def test_dijkstra_with_assets(self):
         graph = Graph(assets_io.read_map(self.map_name))
-        scens = assets_io.read_scenarios(self.map_name)
-        shuffle(scens)
-        dijkstra(scens[0]['start'], scens[0]['goal'], graph)
-        self.assertLessEqual(graph.visited[scens[0]['goal']][0],
-                             scens[0]['shortest'])
+        scen = choice(assets_io.read_scenarios(self.map_name))
+        dijkstra(scen['start'], scen['goal'], graph)
+        result = round(graph.visited[scen['goal']][0], 8)
+        self.assertLessEqual(result,
+                             scen['shortest'])
 
     def test_drawing_found_path(self):
         graph = Graph(assets_io.read_map(self.map_name))
-        scens = assets_io.read_scenarios(self.map_name)
-        shuffle(scens)
-        dijkstra(scens[0]['start'], scens[0]['goal'], graph)
+        scen = choice(assets_io.read_scenarios(self.map_name))
+        dijkstra(scen['start'], scen['goal'], graph)
         file_idx = randint(0,1000000)
-        assets_io.draw_path_onto_map(self.map_name,file_idx,scens[0],graph.visited)
+        assets_io.draw_path_onto_map(self.map_name,file_idx,scen,graph.visited)
         file_formed = os.path.isfile(f'src/output/{self.map_name}_{file_idx}.png')
         self.assertEqual(file_formed,True)
